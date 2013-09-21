b22c16d3b7e8685fe43bff04aeac5d83ba681b2a
arska <arska@10.0.0.101>
2013-09-21 14:49:41 +0300

    Guestin template window done

---
+++ src/mygame/GUI/WindowMaker.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.GUI;
+
+import de.lessvoid.nifty.Nifty;
+
+/**
+ *
+ * @author arska
+ */
+public class WindowMaker {
+    Nifty nifty;
+    public WindowMaker(Nifty nifty){
+        this.nifty=nifty;
+      
+    }
+    public void createGuestWindow(){
+        // create a window
+ 
+    }
+    
+}
